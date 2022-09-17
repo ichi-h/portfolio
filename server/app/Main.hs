ac4c3e1dd20456b85dfd97b03d818b2a3dabd2f1
ichi-h <85932406+ichi-h@users.noreply.github.com>
2022-09-17 21:12:40 +0900

    Introduction of Servant

--- server/app/Main.hs
+++ server/app/Main.hs
@@ -1,10 +1,6 @@
 module Main (main) where
 
-import Control.Concurrent
 import Lib
 
 main :: IO ()
-main = do
-  someFunc
-  threadDelay 10000000
-  main
+main = startApp
--- server/src/Lib.hs
+++ server/src/Lib.hs
@@ -1,6 +1,40 @@
+{-# LANGUAGE DataKinds       #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeOperators   #-}
 module Lib
-    ( someFunc
+    ( startApp
+    , app
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Data.Aeson
+import Data.Aeson.TH
+import Network.Wai
+import Network.Wai.Handler.Warp
+import Servant
+
+data User = User
+  { userId        :: Int
+  , userFirstName :: String
+  , userLastName  :: String
+  } deriving (Eq, Show)
+
+$(deriveJSON defaultOptions ''User)
+
+type API = "api" :> "v1" :> "users" :> Get '[JSON] [User]
+
+startApp :: IO ()
+startApp = run 8080 app
+
+app :: Application
+app = serve api server
+
+api :: Proxy API
+api = Proxy
+
+server :: Server API
+server = return users
+
+users :: [User]
+users = [ User 1 "Isaac" "Newton"
+        , User 2 "Albert" "Einstein"
+        ]
