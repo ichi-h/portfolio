22cc262884e5706f9bf016effdcb5ab5ad4db82b
ichi-h <85932406+ichi-h@users.noreply.github.com>
2022-10-23 23:31:09 +0900

    Define module

--- server/src/Commands/Migrate.hs
+++ server/src/Commands/Migrate.hs
@@ -1,12 +1,14 @@
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
 
+module Commands.Migrate (migrate) where
+
 import Control.Exception
-import Domain.Infrastructures.Beam.PortfolioDB (portfolioDB)
 import Database.Beam.Migrate.Simple (VerificationResult (..), createSchema, verifySchema)
 import Database.Beam.Sqlite
 import Database.Beam.Sqlite.Migrate (migrationBackend)
 import Database.SQLite.Simple
+import Domain.Infrastructures.Beam.PortfolioDB (portfolioDB)
 
 migrate :: IO ()
 migrate = bracket (open "./db/portfolio.db") close $ \conn ->
