c4de74f4c0eeff3bdc422413f3bc030887c22713
ichi-h <85932406+ichi-h@users.noreply.github.com>
2022-12-30 02:54:28 +0900

    Add Anchor

---
+++ client/ui/parts/text/anchor.tsx
@@ -0,0 +1,40 @@
+import { styled } from "linaria/react";
+
+import { THEME } from "../../base";
+import { Color, Size } from "../../types";
+
+interface Props {
+  children: React.ReactNode;
+  href: string;
+  blank?: boolean;
+  color?: Color;
+  fontSize?: Size;
+}
+
+const defaultProps = {
+  color: THEME.color.blue["500"],
+  fontSize: THEME.size.sm,
+};
+
+export const StyledAnchor = styled.a<Props>`
+  color: ${(props) => props.color || defaultProps.color};
+  text-decoration: none;
+  font-size: ${(props) => props.fontSize || defaultProps.fontSize};
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+export const Anchor = (props: Props) => {
+  return (
+    <StyledAnchor
+      href={props.href}
+      target={props.blank ? "_blank" : "_self"}
+      rel={props.blank ? "noopener noreferrer" : ""}
+      color={props.color}
+      fontSize={props.fontSize}
+    >
+      {props.children}
+    </StyledAnchor>
+  );
+};
