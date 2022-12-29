29c77cb3871d95d2f614c0905d2d0e42cb2d88d3
ichi-h <85932406+ichi-h@users.noreply.github.com>
2022-12-30 04:14:24 +0900

    Implement Stack component

---
+++ client/ui/parts/stack/stack.tsx
@@ -0,0 +1,38 @@
+import { styled } from "linaria/react";
+
+import { Size } from "../../types";
+
+interface Props {
+  children: React.ReactNode;
+  justify?: "center" | "flex-start" | "flex-end" | "space-between";
+  align?: "center" | "flex-start" | "flex-end" | "baseline";
+  direction?: "row" | "column";
+  gap?: Size;
+  width?: string;
+  height?: string;
+}
+
+const StyledStack = styled.div<Props>`
+  display: flex;
+  justify-content: ${(props) => props.justify || "center"};
+  align-items: ${(props) => props.align || "center"};
+  flex-direction: ${(props) => props.direction || "row"};
+  gap: ${(props) => props.gap || "0"};
+  width: ${(props) => props.width || "100%"};
+  height: ${(props) => props.height || "100%"};
+`;
+
+export const Stack = (props: Props) => {
+  return (
+    <StyledStack
+      justify={props.justify}
+      align={props.align}
+      direction={props.direction}
+      gap={props.gap}
+      width={props.width}
+      height={props.height}
+    >
+      {props.children}
+    </StyledStack>
+  );
+};
