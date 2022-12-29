49728f820b349100dda740e3d1286ddbf60f57cf
ichi-h <85932406+ichi-h@users.noreply.github.com>
2022-12-30 01:45:46 +0900

    Add linaria and reset css

--- client/next.config.js
+++ client/next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
+const withLinaria = require('next-linaria')
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 };
 
-module.exports = nextConfig;
+const linariaOption = {}
+
+module.exports = withLinaria({
+  ...nextConfig,
+  linaria: linariaOption,
+});
