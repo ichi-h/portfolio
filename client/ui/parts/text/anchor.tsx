8da1915e1777b20952b8b16919c8207260e9e404
ichi-h <85932406+ichi-h@users.noreply.github.com>
2022-12-30 15:16:29 +0900

    Add next/link anchor

--- client/ui/parts/text/anchor.tsx
+++ client/ui/parts/text/anchor.tsx
@@ -1,31 +1,63 @@
 import { styled } from "linaria/react";
+import Link from "next/link";
 
 import { THEME } from "@/ui/base";
 import { Color, Size } from "@/ui/types";
 
-interface Props {
-  children: React.ReactNode;
-  href: string;
-  blank?: boolean;
+interface StyleProps {
   color?: Color;
   fontSize?: Size;
 }
+
+interface AnchorProps extends StyleProps {
+  type: "anchor";
+  children?: React.ReactNode;
+  href: string;
+  blank?: boolean;
+}
+
+interface LinkProps extends StyleProps {
+  type: "link";
+  children?: React.ReactNode;
+  to: string;
+}
+
+type Props = AnchorProps | LinkProps;
 
 const defaultProps = {
   color: THEME.color.blue["500"],
   fontSize: THEME.size.sm,
 };
 
-export const StyledAnchor = styled.a<Props>`
+export const StyledSpan = styled.span<StyleProps>`
   color: ${(props) => props.color || defaultProps.color};
   text-decoration: none;
   font-size: ${(props) => props.fontSize || defaultProps.fontSize};
+  cursor: pointer;
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+export const StyledAnchor = styled.a<StyleProps>`
+  color: ${(props) => props.color || defaultProps.color};
+  text-decoration: none;
+  font-size: ${(props) => props.fontSize || defaultProps.fontSize};
+  cursor: pointer;
   &:hover {
     text-decoration: underline;
   }
 `;
 
 export const Anchor = (props: Props) => {
+  if (props.type === "link") {
+    return (
+      <Link href={props.to}>
+        <StyledSpan color={props.color} fontSize={props.fontSize}>
+          {props.children}
+        </StyledSpan>
+      </Link>
+    );
+  }
   return (
     <StyledAnchor
       href={props.href}
