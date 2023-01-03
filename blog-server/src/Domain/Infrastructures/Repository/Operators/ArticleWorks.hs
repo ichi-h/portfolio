a0fc6cd23afdec929e06d0599f45d02362a52fc7
ichi-h <85932406+ichi-h@users.noreply.github.com>
2023-01-03 19:25:48 +0900

    Implement readAllArticleWorks_

--- blog-server/src/Domain/Infrastructures/Repository/Operators/ArticleWorks.hs
+++ blog-server/src/Domain/Infrastructures/Repository/Operators/ArticleWorks.hs
@@ -5,8 +5,17 @@ module Domain.Infrastructures.Repository.Operators.ArticleWorks
   )
 where
 
-import Database.SQLite.Simple (Connection, Only (..), query_)
-import Domain.Entities.Work (ArticleWork, Work (..))
+import Data.Text (pack)
+import Database.SQLite.Simple (Connection, Query (..), query_)
+import Domain.Entities.Work (ArticleWork)
+import Domain.Infrastructures.Repository.Records.ArticleWork (ArticleWorkR (..), articleWorkRToEntity)
 
 readAllArticleWorks_ :: Connection -> IO [ArticleWork]
-readAllArticleWorks_ conn = undefined
+readAllArticleWorks_ conn = do
+  let select = "SELECT w.*, a.body, t2.* from works w"
+      joinArticles = "INNER JOIN articles a ON w.id = a.work_id"
+      joinTaggings = "INNER JOIN taggings t1 ON w.id = t1.work_id"
+      joinTags = "INNER JOIN tags t2 ON t1.tag_id = t2.id"
+      q = Query {fromQuery = pack $ select ++ " " ++ joinArticles ++ " " ++ joinTaggings ++ " " ++ joinTags}
+  records <- query_ conn q :: IO [ArticleWorkR]
+  pure $ articleWorkRToEntity records
--- blog-server/src/Domain/Infrastructures/Repository/Records/ArticleWork.hs
+++ blog-server/src/Domain/Infrastructures/Repository/Records/ArticleWork.hs
@@ -1,19 +1,72 @@
-module Domain.Infrastructures.Repository.Records.ArticleWork (ArticleWorkR (..)) where
+module Domain.Infrastructures.Repository.Records.ArticleWork (ArticleWorkR (..), articleWorkRToEntity) where
 
+import Data.Text (Text)
+import Data.Time (UTCTime)
 import Database.SQLite.Simple
   ( FromRow (..),
     ToRow (..),
   )
 import Database.SQLite.Simple.FromRow (field)
+import Domain.Entities.Article (Article (..))
+import Domain.Entities.Tag (Tag (..))
+import Domain.Entities.Work (ArticleWork, Work (..))
+import Prelude
 
 data ArticleWorkR = ArticleWorkR
   { articleWorkRId :: Int,
-    articleWorkRName :: String,
-    articleWorkRBatch :: Int
+    articleWorkRCategory :: Text,
+    articleWorkRSlug :: Text,
+    articleWorkRTitle :: Text,
+    articleWorkRDescription :: Text,
+    articleWorkRThumbnailUrl :: Text,
+    articleWorkRCreatedAt :: UTCTime,
+    articleWorkRRevisedAt :: UTCTime,
+    articleWorkRPublishedAt :: Maybe UTCTime,
+    articleWorkRUnpublishedAt :: Maybe UTCTime,
+    articleWorkRIsDraft :: Int,
+    articleWorkRBody :: Text,
+    articleWorkRTagId :: Int,
+    articleWorkRTagName :: Text
   }
 
 instance FromRow ArticleWorkR where
-  fromRow = ArticleWorkR <$> field <*> field <*> field
+  fromRow = ArticleWorkR <$> field <*> field <*> field <*> field <*> field <*> field <*> field <*> field <*> field <*> field <*> field <*> field <*> field <*> field
 
 instance ToRow ArticleWorkR where
-  toRow (ArticleWorkR x y z) = toRow (x, y, z)
+  toRow (ArticleWorkR a b c d e f g h i j k l m o) = toRow (a, b, c, d, e, f, g, h, i, j) ++ toRow (k, l, m, o)
+
+articleWorkRToTag :: [ArticleWorkR] -> [Tag]
+articleWorkRToTag records = map (\r -> Tag (articleWorkRTagId r) (articleWorkRTagName r)) records
+
+rmDuplication :: [ArticleWorkR] -> [ArticleWorkR]
+rmDuplication [] = []
+rmDuplication (x : xs) = x : rmDuplication (filter (\y -> articleWorkRId x /= articleWorkRId y) xs)
+
+articleWorkRToEntity :: [ArticleWorkR] -> [ArticleWork]
+articleWorkRToEntity records =
+  map
+    ( \r ->
+        Work
+          { workId = articleWorkRId r,
+            workCategory = articleWorkRCategory r,
+            workSlug = articleWorkRSlug r,
+            workTitle = articleWorkRTitle r,
+            workDescription = articleWorkRDescription r,
+            workThumbnailUrl = articleWorkRThumbnailUrl r,
+            workCreatedAt = articleWorkRCreatedAt r,
+            workRevisedAt = articleWorkRRevisedAt r,
+            workPublishedAt = articleWorkRPublishedAt r,
+            workUnpublishedAt = articleWorkRUnpublishedAt r,
+            workIsDraft = articleWorkRIsDraft r == 1,
+            workTags = articleWorkRToTag (filter (\y -> articleWorkRId r == articleWorkRId y) records),
+            workContent =
+              Article
+                { articleId = articleWorkRId r,
+                  articleWorkId = articleWorkRId r,
+                  articleBody = articleWorkRBody r
+                }
+          }
+    )
+    uniques
+  where
+    uniques = rmDuplication records
