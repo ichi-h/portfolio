83c177541b5d9aae8d73a5663b68cc8f353c8ee0
ichi-h <85932406+ichi-h@users.noreply.github.com>
2023-01-07 09:28:08 +0900

    Add GlobalNav component

---
+++ client/ui/components/global-nav.tsx
@@ -0,0 +1,56 @@
+import { Box } from "@/ui/parts/box/box";
+import { Stack } from "@/ui/parts/stack/stack";
+import { Link } from "@/ui/parts/text/link";
+
+import { THEME } from "../base";
+
+export const GlobalNav = () => {
+  return (
+    <Box
+      type="nav"
+      width="100%"
+      padding={`${THEME.size.md} ${THEME.size.none}`}
+    >
+      <Stack justify="center">
+        <Stack
+          justify="space-between"
+          align="center"
+          width={THEME.size.pcMaxWidth}
+        >
+          <Box>
+            <Link
+              to="/"
+              fontSize={THEME.size.xl3}
+              color={THEME.color.mono[900]}
+            >
+              ichi-h.com
+            </Link>
+          </Box>
+          <Stack gap={THEME.size.lg}>
+            <Link
+              to="/works"
+              fontSize={THEME.size.xl3}
+              color={THEME.color.mono[900]}
+            >
+              Works
+            </Link>
+            <Link
+              to="/me"
+              fontSize={THEME.size.xl3}
+              color={THEME.color.mono[900]}
+            >
+              Me
+            </Link>
+            <Link
+              to="/contact"
+              fontSize={THEME.size.xl3}
+              color={THEME.color.mono[900]}
+            >
+              Contact
+            </Link>
+          </Stack>
+        </Stack>
+      </Stack>
+    </Box>
+  );
+};
