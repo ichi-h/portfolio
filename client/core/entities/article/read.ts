7dde819a64f013add616901ac47b5c4f594886e3
ichi-h <85932406+ichi-h@users.noreply.github.com>
2023-01-29 11:14:36 +0900

    Remove unused file

--- client/core/entities/article/read.ts
+++
@@ -1,5 +0,0 @@
-import { Article } from "./schema";
-
-export type ReadAllArticleSlugs = () => string[];
-export type ReadAllArticles = () => Article[];
-export type ReadArticleBySlug = (slug: string) => Article;
