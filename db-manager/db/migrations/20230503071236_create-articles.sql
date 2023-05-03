8ed57cac1231108e9a34cca0385b943d66d3ec61
ichi-h <85932406+ichi-h@users.noreply.github.com>
2023-05-03 19:10:51 +0900

    Add migration files

---
+++ db-manager/db/migrations/20230503071236_create-articles.sql
@@ -0,0 +1,12 @@
+-- migrate:up
+
+create table articles (
+  id integer primary key,
+  work_id integer not null,
+  body text not null default '',
+  FOREIGN KEY (`work_id`) REFERENCES `works` (`id`) ON DELETE CASCADE
+);
+
+-- migrate:down
+
+drop table if exists articles;
---
+++ db-manager/db/migrations/20230503071751_create-works.sql
@@ -0,0 +1,18 @@
+-- migrate:up
+
+create table works (
+  id integer primary key,
+  category text not null default '',
+  slug text not null unique default '',
+  title text not null default '',
+  description text not null default '',
+  thumbnail_url text not null default '',
+  created_at datetime not null default CURRENT_TIMESTAMP,
+  revised_at datetime not null default CURRENT_TIMESTAMP,
+  published_at datetime default null,
+  unpublished_at datetime default null
+);
+
+-- migrate:down
+
+drop table if exists works;
