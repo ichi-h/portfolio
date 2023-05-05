5747ed5b6c2f9a33caa286618c71fb5654980198
ichi-h <85932406+ichi-h@users.noreply.github.com>
2023-05-05 16:52:44 +0900

    Add copyright for 404 page

--- node/packages/blog/pages/404.tsx
+++ node/packages/blog/pages/404.tsx
@@ -1,7 +1,9 @@
 import Head from "next/head";
 
+import { COPY_WRITE } from "@/constants/copyright";
 import { THEME } from "@/ui/base";
 import { DefaultLayout } from "@/ui/components/layouts";
+import { Box } from "@/ui/parts/box/box";
 import { Stack } from "@/ui/parts/stack/stack";
 import { Headline } from "@/ui/parts/text/headline";
 import { Link } from "@/ui/parts/text/link";
@@ -31,6 +33,15 @@ const Custom404: NextPageWithLayout = () => {
         <Link to="/" color={THEME.color.mono["900"]} fontSize={THEME.size.md}>
           ホームに戻る
         </Link>
+        <Box
+          type="footer"
+          position="absolute"
+          bottom={THEME.size.md}
+          width="100%"
+          align="center"
+        >
+          <Text color={THEME.color.mono["900"]}>{COPY_WRITE}</Text>
+        </Box>
       </Stack>
     </>
   );
