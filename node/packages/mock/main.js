3f33b8c0ad64ebaecb2538e01bfe8917b55b2724
ichi-h <85932406+ichi-h@users.noreply.github.com>
2024-01-01 01:08:41 +0900

    chore(schemas): separate directories by service

--- node/packages/mock/main.js
+++ node/packages/mock/main.js
@@ -1,7 +1,7 @@
 import { SwaggerUIBundle } from "swagger-ui-dist";
 import "swagger-ui-dist/swagger-ui.css";
 
-import openapi from "../../../schemas/openapi.yaml";
+import openapi from "../../../schemas/works/openapi.yaml";
 
 SwaggerUIBundle({
   url: openapi,
