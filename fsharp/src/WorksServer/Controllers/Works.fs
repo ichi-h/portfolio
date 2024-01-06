8abd007485016ff927df44aff86c13eb30852361
ichi-h <85932406+ichi-h@users.noreply.github.com>
2024-01-06 19:47:04 +0900

    feat(works-server): implement request validation in controller

--- fsharp/src/WorksServer/Controllers/Works.fs
+++ fsharp/src/WorksServer/Controllers/Works.fs
@@ -1,14 +1,16 @@
 module WorksServer.Controllers.Works
 
-open System
 open FsToolkit.ErrorHandling
 open Microsoft.AspNetCore.Http
 open Microsoft.AspNetCore.Mvc
+open WorksServer.Controllers
 open WorksServer.Gateway.Database.Queries.Works.Filter
 open WorksServer.UseCases.Works.Filter.Input
 open WorksServer.UseCases.Works.Filter.Interactor
-open WorksServer.Utils.Nullable
+open WorksServer.UseCases.Works.Filter.Output
 open WorksServer.Values.Category
+open WorksServer.Values.LimitNumber
+open WorksServer.Values.Offset
 
 type FilterRequest =
     { [<FromQuery>]
@@ -21,47 +23,48 @@ type FilterRequest =
       limit: string }
 
 let filter (request: FilterRequest) =
-    // TODO: refactor
-    let search = nullToOptional request.search
-
-    // TODO: refactor
-    let category =
-        match nullToOptional request.category with
-        | Some value -> parseCategory value |> Some
-        | None -> None
-
-    // TODO: refactor
-    let offset =
-        match nullToDefaultValue request.offset "-1"
-              |> Int16.TryParse
-            with
-        | (true, value) -> int value
-        | _ -> -1
-
-    // TODO: refactor
-    let limit =
-        match nullToDefaultValue request.limit "-1"
-              |> Int16.TryParse
-            with
-        | (true, value) -> int value
-        | _ -> -1
-
-    let response =
+    let outputResult =
         result {
-            let! input =
-                validateFilterInput
-                    { filterWorks = filterWorks
-                      search = search
-                      category = category
-                      offset = offset
-                      limit = limit }
+            let search =
+                Validations.openWith "search" request.search
+                |> Validations.Option.parse
+                |> Validations.close
 
-            let output = filterWorksInteractor input
+            let category =
+                Validations.openWith "category" request.category
+                |> Validations.Option.parse
+                |> Validations.close
+                |> Option.map createCategory
 
-            return! Ok output
+            let! offset =
+                Validations.openWith "offset" request.offset
+                |> Validations.Required.isExist
+                |> Result.bind Validations.Number.isInt
+                |> Result.map Validations.close
+                |> Result.bind createOffset
+
+            let! limit =
+                Validations.openWith "limit" request.limit
+                |> Validations.Required.isExist
+                |> Result.bind Validations.Number.isInt
+                |> Result.map Validations.close
+                |> Result.bind (createLimitNumber (Some 1) (Some 50))
+
+            let input =
+                { filterWorks = filterWorks
+                  search = search
+                  category = category
+                  offset = offset
+                  limit = limit }
+
+            return! Ok(filterWorksInteractor input)
         }
 
-    // TODO: refactor
-    match response with
-    | Ok value -> Results.Ok value
-    | Error error -> Results.Ok error
+    match outputResult with
+    | Ok output ->
+        match output with
+        | Ok success -> Results.Ok success
+        | Error failure ->
+            match failure with
+            | InfrastructureError message -> Results.Problem "Internal Server Error"
+    | Error e -> Results.UnprocessableEntity e
