9b04d9c70136aa6d32b7a17cce7e27fcfbc9b64b
ichi-h <85932406+ichi-h@users.noreply.github.com>
2024-01-08 13:11:32 +0900

    feat(works-client): add work apis

---
+++ node/packages/works/api/works/filter.ts
@@ -0,0 +1,23 @@
+import { Category } from "@/model/category";
+import { LimitNumber } from "@/model/limitNumber";
+import { Offset } from "@/model/offset";
+import { SummarizedWork, Work } from "@/model/work";
+import { joinQueryParams } from "@/utils/queryParams";
+
+import { customFetch } from "../customFetch";
+import { APIResult } from "../result";
+
+export const getFilteredWorks = async (
+  query: {
+    search?: string;
+    category?: Category;
+    offset: Offset;
+    limit: LimitNumber;
+  },
+  receive: (work: APIResult<SummarizedWork[]>) => void
+): Promise<void> => {
+  const work = await customFetch<SummarizedWork[]>(
+    `/api/works/filter${joinQueryParams(query)}`
+  );
+  receive(work);
+};
---
+++ node/packages/works/api/works/show.ts
@@ -0,0 +1,12 @@
+import { Work } from "@/model/work";
+
+import { customFetch } from "../customFetch";
+import { APIResult } from "../result";
+
+export const getWork = async (
+  slug: string,
+  receive: (work: APIResult<Work>) => void
+): Promise<void> => {
+  const work = await customFetch<Work>(`/api/works/${slug}`);
+  receive(work);
+};
