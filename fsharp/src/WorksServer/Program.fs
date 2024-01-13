64d1d3275b61eea4e585504e30f4625caa6e1429
ichi-h <85932406+ichi-h@users.noreply.github.com>
2024-01-14 00:19:03 +0900

    feat(works-server): apply cors

--- fsharp/src/WorksServer/Program.fs
+++ fsharp/src/WorksServer/Program.fs
@@ -1,16 +1,34 @@
 ﻿open DotNetEnv
 open Microsoft.AspNetCore.Builder
+open Microsoft.Extensions.DependencyInjection
 open WorksServer.Routes
 
 [<EntryPoint>]
 let main args =
+    Env.Load() |> ignore
+
     let builder = WebApplication.CreateBuilder(args)
+
+    builder.Services.AddCors() |> ignore
+
     let app = builder.Build()
+
+    let clientOrigin = Env.GetString("CLIENT_ORIGIN")
+
+    app.UseCors (fun builder ->
+        builder
+            .WithOrigins(clientOrigin)
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+
+        |> ignore
+
+        ())
+    |> ignore
 
     app |> routes.Apply |> ignore
 
-    Env.Load() |> ignore
-    let origin = Env.GetString("APP_ORIGIN")
-    app.Run(origin)
+    let appOrigin = Env.GetString("APP_ORIGIN")
+    app.Run(appOrigin)
 
     0 // Exit code
