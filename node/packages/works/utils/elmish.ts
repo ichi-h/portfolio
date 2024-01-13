f4fe6081d4956edfac67e9949d0f5a8661be2ad9
ichi-h <85932406+ichi-h@users.noreply.github.com>
2024-01-13 10:19:38 +0900

    feat(node): Add cmd to init with nullable

--- node/packages/works/utils/elmish.ts
+++ node/packages/works/utils/elmish.ts
@@ -9,33 +9,57 @@ export type Update<Model, Msg extends BaseMessage> = (
 
 export type UpdateView<Model> = (model: Model) => void;
 
+export type Init<Model, Msg extends BaseMessage> =
+  | Model
+  | (() => { model: Model; cmd?: () => Promise<Msg> });
+
 export type UseUpdate<Model, Msg extends BaseMessage> = (
-  model: Model,
+  init: Init<Model, Msg>,
   updateView: (state: Model) => void,
 ) => Send<Msg>;
+
+type Store<Model> = {
+  initialized: boolean;
+  model: Model | null;
+};
+
+const deepClone = <T>(obj: T): T => JSON.parse(JSON.stringify(obj));
+
+const isModel = <Model, Msg extends BaseMessage>(
+  init: Init<Model, Msg>,
+): init is Model => typeof init !== "function";
 
 export const createUpdate = <Model, Msg extends BaseMessage>(
   update: Update<Model, Msg>,
 ): UseUpdate<Model, Msg> => {
-  let storedModel: Model | null = null;
+  const store: Store<Model> = {
+    initialized: false,
+    model: null,
+  };
 
-  const useUpdate =
-    (init: Model, updateView: UpdateView<Model>) => (message: Msg) => {
-      if (!storedModel) storedModel = init;
-      const model = storedModel;
-
-      const { newModel, cmd } = update(model, message);
-      storedModel = newModel;
-
+  const useUpdate = (init: Init<Model, Msg>, updateView: UpdateView<Model>) => {
+    const send = (message: Msg) => {
+      const { newModel, cmd } = update(
+        deepClone(store.model as Model),
+        message,
+      );
+      store.model = newModel;
       updateView(newModel);
-
-      if (cmd) {
-        cmd().then((msg) => {
-          const send = useUpdate(newModel, updateView);
-          send(msg);
-        });
-      }
+      if (cmd) cmd().then(send);
     };
+
+    if (!store.initialized) {
+      const { model: initModel, cmd: initCmd } = isModel(init)
+        ? { model: init, cmd: undefined }
+        : init();
+
+      store.initialized = true;
+      store.model = initModel;
+      if (initCmd) initCmd().then(send);
+    }
+
+    return send;
+  };
 
   return useUpdate;
 };
