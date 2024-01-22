d8f92645d292fb5513a8cca9395250d80ccb34f6
ichi-h <85932406+ichi-h@users.noreply.github.com>
2024-01-23 00:05:42 +0900

    chore(ui): git mv index.ts src

--- node/packages/ui/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./src/components";
-export * from "./src/styles";
-export * from "./src/constants";
-
-export const version = "0.0.0";
---
+++ node/packages/ui/src/index.ts
@@ -0,0 +1,5 @@
+export * from "./components";
+export * from "./styles";
+export * from "./constants";
+
+export const version = "0.0.0";
