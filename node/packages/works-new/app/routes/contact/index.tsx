21051abbd0746cff31e4845724431a43e226ac72
ichi-h <85932406+ichi-h@users.noreply.github.com>
2024-02-11 20:31:48 +0900

    feat(node/works-new): temporarily  implement /contact page

---
+++ node/packages/works-new/app/routes/contact/index.tsx
@@ -0,0 +1,17 @@
+import { Text } from "portfolio-ui";
+
+import { Layout } from "@/components/layout";
+import { Title } from "@/components/title";
+
+export default function Index() {
+  return (
+    <Layout>
+      <div>
+        <Title>Contact</Title>
+        <Text fontSize="4" color="mono.900">
+          Coming soon...
+        </Text>
+      </div>
+    </Layout>
+  );
+}
