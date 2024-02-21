6dcfbc3d3376c038848fdf2c5bc8adfc020c07d4
ichi-h <85932406+ichi-h@users.noreply.github.com>
2024-02-21 21:15:35 +0900

    feat(db-manager): update entrypoint

--- db-manager/entrypoint.sh
+++ db-manager/entrypoint.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/sh
 
-rm -f ./portfolio.sqlite3
-litestream restore -config ./litestream.yaml ./portfolio.sqlite3
-litestream replicate -config ./litestream.yaml
+if [ "$1" = "dev" ]; then
+  dbmate create
+  dbmate migrate
+  sqlite3 portfolio.sqlite3 < seeds.sql
+elif [ "$1" = "prod" ]; then
+  litestream restore -config ./litestream.yaml ./portfolio.sqlite3
+  dbmate migrate
+  litestream replicate -config ./litestream.yaml
+fi
