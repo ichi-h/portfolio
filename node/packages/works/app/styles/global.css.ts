cf45de81525afa0e57b942e1e07cef9f739f70de
ichi-h <85932406+ichi-h@users.noreply.github.com>
2024-03-17 15:04:27 +0900

    feat(node/works): update code font-family

--- node/packages/works/app/styles/global.css.ts
+++ node/packages/works/app/styles/global.css.ts
@@ -3,12 +3,21 @@ import { bg, m } from "portfolio-styles";
 
 export const html = style([bg["mono.900"], m[0]]);
 
-globalStyle(`${html} *`, {
+globalStyle(`${html}`, {
   fontFamily: '"Zen Kaku Gothic New", sans-serif',
+  fontStyle: "normal",
 });
+
+globalStyle(
+  `${html} button, ${html} input, ${html} optgroup, ${html} select, ${html} textarea`,
+  {
+    fontFamily: "inherit",
+  },
+);
 
 globalStyle(`${html} body`, m[0]);
 
 globalStyle(`${html} code`, {
-  fontFamily: "monospace",
+  fontFamily:
+    '"SFMono-Regular", Consolas, "Liberation Mono", Menlo, monospace, "Segoe UI Emoji"',
 });
