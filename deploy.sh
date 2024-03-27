29723d17c2a26fe97fe06b09d3557a0251d01cb8
ichi-h <85932406+ichi-h@users.noreply.github.com>
2024-03-27 21:07:57 +0900

    chore: update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 source .env
 git pull
 
-docker-compose -f docker-compose_prod.yaml down -v
+docker-compose -f docker-compose_prod.yaml down
 docker-compose -f docker-compose_prod.yaml pull
 docker-compose -f docker-compose_prod.yaml up -d
 
