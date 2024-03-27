9757337fda2f9b637627876bda94c38450315c47
ichi-h <85932406+ichi-h@users.noreply.github.com>
2024-03-27 21:07:29 +0900

    chore: update push-ecr script

--- push-ecr.sh
+++ push-ecr.sh
@@ -1,7 +1,6 @@
 source ./.env
-docker-compose -f docker-compose_prod-build.yaml build
+docker-compose -f docker-compose_prod-build.yaml build --no-cache
 docker tag ${AWS_ACCOUNT_ID}.dkr.ecr.ap-northeast-1.amazonaws.com/portfolio-db-manager:latest ${AWS_ACCOUNT_ID}.dkr.ecr.ap-northeast-1.amazonaws.com/portfolio-db-manager:latest
-docker tag ${AWS_ACCOUNT_ID}.dkr.ecr.ap-northeast-1.amazonaws.com/portfolio-db-client:latest ${AWS_ACCOUNT_ID}.dkr.ecr.ap-northeast-1.amazonaws.com/portfolio-db-client:latest
 docker tag ${AWS_ACCOUNT_ID}.dkr.ecr.ap-northeast-1.amazonaws.com/portfolio-works-server:latest ${AWS_ACCOUNT_ID}.dkr.ecr.ap-northeast-1.amazonaws.com/portfolio-works-server:latest
 docker tag ${AWS_ACCOUNT_ID}.dkr.ecr.ap-northeast-1.amazonaws.com/portfolio-nginx:latest ${AWS_ACCOUNT_ID}.dkr.ecr.ap-northeast-1.amazonaws.com/portfolio-nginx:latest
 docker-compose -f docker-compose_prod-build.yaml push
